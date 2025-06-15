b006ee905a0bde1ff2b954bdfc0ea2e294ca89f9
Ashot Kostanyan <kostanyanashot2007@gmail.com>
2025-06-16 01:39:51 +0400

    helm template adding

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
-    host: proccess.env.DB_HOST,
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
